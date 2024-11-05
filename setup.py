320073ded15eaae9cfb62a7fc615e4102771f4b2
SB <swarnendubanerjee123@gmail.com>
2024-11-05 12:29:36 +0530

    ssec comm

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Project',
+    version= '0.0.0',
+    author= 'SB',
+    author_email= 'banerjeeswarnendu6@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
